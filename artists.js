fa73fbad7af75c88a002d76b1834f28cde3f9f35
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-10 00:56:03 -0300

    agrego getTitle() que scrapea el sitio y devuelve el titulo

--- artists.js
+++ artists.js
@@ -15,3 +15,18 @@ const getArtist = function(page) {
 		return firstArtist;
 	}
 }
+
+const getTitle = function(page) {
+	if(page === "spotify") {
+		const selector = "a[aria-label*='Now playing:']";
+		const tag = document.querySelector(selector);
+		if(!tag) return;
+		const playingTrack = tag.ariaLabel;
+		const m = playingTrack.match(/Now playing: (?<title>.+) by (?<artists>.+)/);
+
+		const title = m.groups.title;
+		const withoutFeat = title.replace(/ \(feat\..*\)/, "");
+
+		return withoutFeat;
+	}
+}
