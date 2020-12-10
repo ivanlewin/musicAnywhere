9a6bd65b08a22b1d894cc6e26bc8fe28b081962f
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-10 00:53:02 -0300

    creo array con los sitios soportados

--- background.js
+++ background.js
@@ -4,6 +4,14 @@
 
 'use strict';
 
+const supportedSites = [
+	{url: "youtube.com", icons: "youtube"},
+	{url: "music.youtube.com", icons: "youtube"},
+	{url: "open.spotify.com", icons: "spotify"},
+	{url: "www.musixmatch.com", icons: "musixmatch"},
+	{url: "genius.com", icons: "genius"},
+	{url: "music.apple.com", icons: "apple-music"}
+]
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('The color is green.');
