fc81125167bac2ea6f83f0ff8d0f3aa2a293b56c
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-10 00:55:53 -0300

    agrego getArtist() que scrapea el sitio y devuelve el artista

--- artists.js
+++ artists.js
@@ -1,3 +1,17 @@
 'use strict';
 console.log("popupp.js");
 
+const getArtist = function(page) {
+	if(page === "spotify") {
+		const selector = "a[aria-label*='Now playing:']";
+		const tag = document.querySelector(selector);
+		if(!tag) return;
+		const playingTrack = tag.ariaLabel;
+		const m = playingTrack.match(/Now playing: (?<title>.+) by (?<artists>.+)/);
+
+		const artists = m.groups.artists;
+		const firstArtist = artists.split(",")[0];
+
+		return firstArtist;
+	}
+}
