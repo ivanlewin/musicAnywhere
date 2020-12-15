7780aa243a13f6e857c7efc72b25ee465ae27056
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-15 00:20:43 -0300

    quito logica de spotify de artists.js

--- artists.js
+++ artists.js
@@ -5,17 +5,6 @@ const getArtist = function(page) {
 	switch(page) {
 import { getTrackInfoAppleMusic, getTrackInfoGenius, getTrackInfoMusixmatch, getTrackInfoSpotfiy, getTrackInfoYoutube, getTrackInfoYoutubeMusic } from "./sites.js";
 
-		case "spotify": {
-			const selector = "a[aria-label*='Now playing:']";
-			const tag = document.querySelector(selector);
-			if(!tag) return;
-			const playingTrack = tag.ariaLabel;
-			const m = playingTrack.match(/Now playing: (?<title>.+) by (?<artists>.+)/);
-	
-			const artists = m.groups.artists;
-			artist = artists.split(",")[0];
-			break;
-		}
 
 		case "youtubeMusic": {
 			const selector = "div.content-info-wrapper.style-scope span.subtitle yt-formatted-string.ytmusic-player-bar";
@@ -69,20 +58,6 @@ import { getTrackInfoAppleMusic, getTrackInfoGenius, getTrackInfoMusixmatch, get
 
 const getTitle = function(page) {
 	let withoutFeat = "";
-	switch(page) {
-
-		case "spotify": {
-			const selector = "a[aria-label*='Now playing:']";
-			const tag = document.querySelector(selector);
-			if(!tag) return;
-			const playingTrack = tag.ariaLabel;
-			const m = playingTrack.match(/Now playing: (?<title>.+) by (?<artists>.+)/);
-
-			const title = m.groups.title;
-			withoutFeat = title.replace(/ \(feat\..*\)/, "");
-
-			break;
-		}
 
 		case "youtubeMusic": {
 			const selector = "div.content-info-wrapper.style-scope > yt-formatted-string.title";
