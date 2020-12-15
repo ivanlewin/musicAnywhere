3858328494546fd0424dfcf0f9f7ceaea23395de
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-15 00:04:11 -0300

    muevo logica de spotify a sites.js

--- sites.js
+++ sites.js
@@ -1,3 +1,18 @@
 const getTrackInfoAppleMusic = function() {
 
 };
+const getTrackInfoSpotfiy = function() {
+
+	const trackInfo = {};
+
+	const tag = document.querySelector("a[aria-label*='Now playing:']");
+	if(!tag) return;
+	const playingTrack = tag.ariaLabel;
+	const m = playingTrack.match(/Now playing: (?<title>.+) by (?<artists>.+)/);
+	if(!m) return;
+
+	trackInfo.artists = m.groups.artists.split(",");
+	trackInfo.title = m.groups.title.replace(/ \(feat\..*\)/, "");
+
+	return trackInfo;
+};
