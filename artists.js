10e44e7bd4f11e8a839eaaac7e7ba2db47e1dee1
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-15 01:10:14 -0300

    agrego getSiteInfo con el switch statement que llama a las funciones por sitio

--- artists.js
+++ artists.js
@@ -25,6 +25,37 @@ const artist = getArtist(page);
 const title = getTitle(page);
 const url = searchIn(page, title, artist);
 console.log("artist: ", artist);
+const getSiteInfo = function() {
+
+	const hostname = new URL(window.location.href).hostname;
+	console.log(hostname)
+
+	switch(hostname) {
+		case "music.apple.com": {
+			return getTrackInfoAppleMusic();
+		}
+		case "genius.com": {
+			return getTrackInfoGenius();
+		}
+		case "www.musixmatch.com": {
+			return getTrackInfoMusixmatch();
+		}
+		case "open.spotify.com": {
+			return getTrackInfoSpotfiy();
+		}
+		case "www.youtube.com": {
+			return getTrackInfoYoutube();
+		}
+		case "music.youtube.com": {
+			return getTrackInfoYoutubeMusic();
+		}
+		default: {
+			return;
+		}
+	}
+};
+
+const {title, artists} = getSiteInfo();
 console.log("title: ", title);
 console.log("url: ", url);
 // window.open(url);
