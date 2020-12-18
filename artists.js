33ebf21d956e0aad63bed17aed247e0879dfe942
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-17 23:52:51 -0300

    muevo searchIn a popup.js

--- artists.js
+++ artists.js
@@ -3,21 +3,7 @@ import { getTrackInfoAppleMusic, getTrackInfoGenius, getTrackInfoMusixmatch, get
 
 // chrome.runtime.getURL("./site.js");
 
-const searchIn = function(site, title, artists) {
-	// Returns a URL to a search query for the song on the given site.
-	const searchURLs = {
-		"appleMusic": "https://music.apple.com/us/search?term=",
-		"genius": "https://genius.com/search?q=",
-		"musixmatch": "https://www.musixmatch.com/search/",
-		"spotify": "https://open.spotify.com/search/",
-		"youtube": "https://www.youtube.com/results?search_query=",
-		"youtubeMusic": "https://music.youtube.com/search?q="
-	}
 
-	artists = artists.join(" ");
-	let searchQuery = `${title} ${artists}`;
-
-	return searchURLs[site] + encodeURI(searchQuery);
 }
 
 const getSiteInfo = function() {
--- popup.js
+++ popup.js
@@ -5,8 +5,22 @@ chrome.tabs.executeScript({
 });
 
 // let changeColor = document.getElementById('changeColor');
+const searchIn = function(site, title, artists) {
+	// Returns a URL to a search query for the song on the given site.
+	const searchURLs = {
+		"appleMusic": "https://music.apple.com/us/search?term=",
+		"genius": "https://genius.com/search?q=",
+		"musixmatch": "https://www.musixmatch.com/search/",
+		"spotify": "https://open.spotify.com/search/",
+		"youtube": "https://www.youtube.com/results?search_query=",
+		"youtubeMusic": "https://music.youtube.com/search?q="
+	}
 
-// chrome.storage.sync.get('color', function(data) {
+	artists = artists.join(" ");
+	let searchQuery = `${title} ${artists}`;
+
+	return searchURLs[site] + encodeURI(searchQuery);
+}
 // 	changeColor.style.backgroundColor = data.color;
 // 	changeColor.setAttribute('value', data.color);
 // });
