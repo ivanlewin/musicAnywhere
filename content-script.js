a0f6b1de36b5fe98af2a18ae250c880274eb9cc4
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-28 11:04:31 -0300

    borro getTrackInfoGenius

--- content-script.js
+++ content-script.js
@@ -145,42 +145,6 @@ const psAppleMusic = function() {
     return trackInfo;
 };
 
-/** Looks for the track info on Genius
- * 
- * @returns {trackInfo}
- */
-const getTrackInfoGenius = function() {
-
-    const trackInfo = {
-        "title": undefined,
-        "artists": []
-    };
-    
-    // Get the primary artist
-    let tag = document.querySelector("a[class*='primary_artist']");
-    if(!tag) return;
-    let primaryArtist = tag.textContent.trim();
-
-    trackInfo.artists.push(primaryArtist);
-
-    // Get the featuring artists
-    let tags = document.querySelectorAll("expandable-list[label='Featuring'] span[ng-repeat*='artist in collection'] a");
-    if(!tags) return;
-    let tagArray = Array.from(tags); // Make an array from the NodeList
-    let featuringArtists = tagArray.map(tag => tag.textContent.trim());
-
-    trackInfo.artists.push(...featuringArtists);
-
-    // Get the track title
-    tag = document.querySelector("h1[class*='primary_info-title']");
-    if(!tag) return;
-    let title = tag.textContent.trim();
-
-    trackInfo.title = title;
-
-    return trackInfo;
-};
-
 /** Looks for the track info on Musixmatch
  * 
  * @returns {trackInfo}
