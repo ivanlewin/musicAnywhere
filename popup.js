8814e2490e817a960dc3eb4a883b05aba2e0b7bb
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-12-28 11:11:55 -0300

    borro event listener de openInDesktopBtn

--- popup.js
+++ popup.js
@@ -70,11 +70,3 @@ linkContainer.addEventListener("click", (e) => {
 });
 
 
-openInDesktopBtn.addEventListener("click", (e) => {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(
-			tabs[0].id,
-			{ function : "getDesktopURI"},
-			response => chrome.tabs.create({ url: response.desktopURI }));
-	});
-});
